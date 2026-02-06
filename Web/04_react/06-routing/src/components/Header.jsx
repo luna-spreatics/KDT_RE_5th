3fbcd2bfb285ce9af1ec650c8bbccb377413f4af
luna-spreatics <luna@spreatics.com>
2026-02-06 14:32:08 +0900

    feat: initialize React routing project with basic structure and components

---
+++ Web/04_react/06-routing/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+	return (
+		<header>
+			<span>Router Tutorial</span>
+			<ul>
+				<li>Home</li>
+				<li>Products</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
